57e48ac28006faa8a1b42ec53b844130b14fd212
Crynowecer0 <crynowecer11@gmail.com>
2023-05-10 14:35:44 -0400

    progress on utils.js

--- app.js
+++ app.js
@@ -3,20 +3,29 @@
 const express = require("express");
 const app = express();
 
+const { findMean, findMedian, findMode } = require("./stats.js");
+const { convertStrNums } = require("./utils.js");
+
 // useful error class to throw
 const { NotFoundError } = require("./expressError");
 
+app.use(express.json());
+
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
-
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
+app.get("/mean", function (req, res) {
+  //potentially use gaurd statement
+  //pull out the numbers using the appropriate request method
+  //req.params.nums -> string at first that we need to convert with 
+  //perform the calculation using the calculation function in stats.js
 
+  //respond with nums
+});
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
 
-
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
-
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res) {
@@ -30,7 +39,5 @@ app.use(function (err, req, res, next) {
   if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
   return res.status(status).json({ error: { message, status } });
 });
-
-
 
 module.exports = app;
--- utils.js
+++ utils.js
@@ -1,12 +1,24 @@
 const { BadRequestError } = require("./expressError");
 
-
 /** Convert strNums like ["1","2","3"] to [1, 2, 3]. */
 
 function convertStrNums(strNums) {
+  let nums = [];
+
+  for (const num of strNums) {
+    if (typeof Number.parseFloat(num) != "integer") {
+      throw new BadRequestError(`${num} is not a number`);
+    }
+    nums.push(Number.parseFloat(num));
+  }
+
+  return nums;
+
   // if the conversion isn't successful, throw a BadRequestError and will
   // be handled in your route
 }
 
+convertStrNums(["1","2","3"])
+convertStrNums(["foo","2","3"])
 
 module.exports = { convertStrNums };
